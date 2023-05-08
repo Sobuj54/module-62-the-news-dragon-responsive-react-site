6c5c92942860511b75d035fe07d5acdd076e2e0a
soubj <sobujahmed124@gmail.com>
2023-05-08 23:55:49 +0600

    loaded data dynamically in all category leftNav

--- src/pages/Shared/LeftNav/LeftNav.jsx
+++ src/pages/Shared/LeftNav/LeftNav.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftNav = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/categories")
+      .then((res) => res.json())
+      .then((data) => setCategories(data))
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <div>
-      <h2>left nav</h2>
+      <h4>All Category</h4>
+
+      <div className="ms-3">
+        {categories.map((category) => (
+          <p key={category.id}>
+            <Link
+              to={`/categories/${category.id}`}
+              className="text-decoration-none text-black">
+              {category.name}
+            </Link>
+          </p>
+        ))}
+      </div>
     </div>
   );
 };
--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -11,16 +11,18 @@ import QZone from "../QZone/QZone";
 
 const RightNav = () => {
   return (
-    <div>
-      <h4>Login With</h4>
-      <Button className="mb-2" variant="outline-primary">
-        <FaGoogle /> Login with Google
-      </Button>
-      <Button variant="outline-secondary">
-        <FaGithub></FaGithub> Login with Github
-      </Button>
-
+    <div className="mt-4">
       <div>
+        <h4>Login With</h4>
+        <Button className="mb-2" variant="outline-primary">
+          <FaGoogle /> Login with Google
+        </Button>
+        <Button variant="outline-secondary">
+          <FaGithub></FaGithub> Login with Github
+        </Button>
+      </div>
+
+      <div className="mt-4">
         <h4>Find us on</h4>
         <ListGroup>
           <ListGroup.Item>
