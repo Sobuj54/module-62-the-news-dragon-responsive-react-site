391533dd36e0a249c2b0a400d308ee4e61660f15
soubj <sobujahmed124@gmail.com>
2023-05-08 21:18:31 +0600

    created nav bar

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "../../../assets/logo.png";
 import moment from "moment/moment";
-import { Button, Container } from "react-bootstrap";
+import { Button, Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
 import Marquee from "react-fast-marquee";
 
 const Header = () => {
@@ -12,15 +12,36 @@ const Header = () => {
         <p className="text-secondary">
           <small>Journalism Without Fear or Favour</small>
         </p>
+        {/* dynamic current date using moment.js */}
         <p>{moment().format("dddd, MMMM Do, YYYY")}</p>
       </div>
       <div className="d-flex">
         <Button variant="danger">Latest</Button>
+        {/* continuously moving text using react marquee*/}
         <Marquee className="text-success" speed={50}>
           I can be a React component, multiple React components, or just some
           text.
         </Marquee>
       </div>
+
+      <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+        <Container>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mx-auto">
+              <Nav.Link href="#features">Home</Nav.Link>
+              <Nav.Link href="#pricing">About</Nav.Link>
+              <Nav.Link href="#pricing">Career</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">Login</Nav.Link>
+              <Nav.Link eventKey={2} href="#memes">
+                <Button variant="secondary">Secondary</Button>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </Container>
   );
 };
