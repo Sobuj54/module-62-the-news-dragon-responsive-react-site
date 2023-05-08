70a653f72e2cb1143edb2b1c602fe2612861b820
soubj <sobujahmed124@gmail.com>
2023-05-08 20:19:18 +0600

    created shared component pages

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { RouterProvider } from "react-router-dom";
-import router from "./routes/Route.js";
+import router from "./routes/Route.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
