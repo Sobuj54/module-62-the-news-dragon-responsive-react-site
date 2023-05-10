e7f6e89c2675be39df0fa79b2289b9e7810936e0
soubj <sobujahmed124@gmail.com>
2023-05-11 00:04:14 +0600

    loaded images and fixed img size

--- src/pages/Home/Home/NewsCard/NewsCard.jsx
+++ src/pages/Home/Home/NewsCard/NewsCard.jsx
@@ -1,13 +1,22 @@
 import React from "react";
+import { Image } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
 import { Link } from "react-router-dom";
 
 const NewsCard = ({ news }) => {
-  const { _id, details, title, image_url } = news;
+  const { _id, details, title, image_url, author } = news;
 
   return (
     <Card className="mb-4">
-      <Card.Header>Featured</Card.Header>
+      <Card.Header className="d-flex">
+        <Image style={{ height: "40px" }} src={author?.img} roundedCircle />
+        <div>
+          <p>{author?.name}</p>
+          <p>
+            <small>{author.published_date}</small>
+          </p>
+        </div>
+      </Card.Header>
       <Card.Body>
         <Card.Title>{title}</Card.Title>
         <Card.Img variant="top" src={image_url} />
