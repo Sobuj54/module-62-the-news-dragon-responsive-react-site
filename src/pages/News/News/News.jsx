11cac30aba0e1dc125ebda87196ccf828783bef2
soubj <sobujahmed124@gmail.com>
2023-05-11 20:13:56 +0600

    showed news details and also can show news in the same category

--- src/pages/News/News/News.jsx
+++ src/pages/News/News/News.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import { Button, Card } from "react-bootstrap";
+import { FaArrowLeft } from "react-icons/fa";
+import { Link, useLoaderData } from "react-router-dom";
 
 const News = () => {
+  const news = useLoaderData();
+  const { category_id, details, title, image_url } = news;
+
   return (
-    <div>
-      <h2>news</h2>
-    </div>
+    <Card>
+      <Card.Img variant="top" src={image_url} />
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>{details}</Card.Text>
+        <Link to={`/categories/${category_id}`}>
+          <Button variant="danger">
+            <FaArrowLeft></FaArrowLeft> All News in this category
+          </Button>
+        </Link>
+      </Card.Body>
+    </Card>
   );
 };
 
