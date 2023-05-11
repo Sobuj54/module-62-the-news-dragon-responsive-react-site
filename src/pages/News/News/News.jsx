efdac634e8b3ccd1b1be37c25871dcc57bfe43f3
soubj <sobujahmed124@gmail.com>
2023-05-11 20:28:35 +0600

    finished editors choice section with cards

--- src/pages/News/News/News.jsx
+++ src/pages/News/News/News.jsx
@@ -2,24 +2,28 @@ import React from "react";
 import { Button, Card } from "react-bootstrap";
 import { FaArrowLeft } from "react-icons/fa";
 import { Link, useLoaderData } from "react-router-dom";
+import EditorsInsights from "../EditorsInsights/EditorsInsights";
 
 const News = () => {
   const news = useLoaderData();
   const { category_id, details, title, image_url } = news;
 
   return (
-    <Card>
-      <Card.Img variant="top" src={image_url} />
-      <Card.Body>
-        <Card.Title>{title}</Card.Title>
-        <Card.Text>{details}</Card.Text>
-        <Link to={`/categories/${category_id}`}>
-          <Button variant="danger">
-            <FaArrowLeft></FaArrowLeft> All News in this category
-          </Button>
-        </Link>
-      </Card.Body>
-    </Card>
+    <div>
+      <Card>
+        <Card.Img variant="top" src={image_url} />
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>{details}</Card.Text>
+          <Link to={`/categories/${category_id}`}>
+            <Button variant="danger">
+              <FaArrowLeft></FaArrowLeft> All News in this category
+            </Button>
+          </Link>
+        </Card.Body>
+      </Card>
+      <EditorsInsights></EditorsInsights>
+    </div>
   );
 };
 
