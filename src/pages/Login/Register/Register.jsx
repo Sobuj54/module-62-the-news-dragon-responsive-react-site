cada59d3afa37b39d410eeb466c3544a41d26321
soubj <sobujahmed124@gmail.com>
2023-05-11 23:53:30 +0600

    created register form

--- src/pages/Login/Register/Register.jsx
+++ src/pages/Login/Register/Register.jsx
@@ -1,10 +1,62 @@
 import React from "react";
+import { Button, Container, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   return (
-    <div>
-      <h2>register</h2>
-    </div>
+    <Container className="w-25 mx-auto">
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="text"
+            name="name"
+            required
+            placeholder="Enter name"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Photo URL</Form.Label>
+          <Form.Control
+            type="text"
+            name="photo"
+            required
+            placeholder="Enter PhotoURL"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            name="email"
+            required
+            placeholder="Enter email"
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            name="password"
+            required
+            placeholder="Password"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Accept Terms & Conditions" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Register
+        </Button>
+        <br />
+        <Form.Text className="text-secondary">
+          Already have an account? <Link to="/login">Login</Link>
+        </Form.Text>
+        <Form.Text className="text-success"></Form.Text>
+        <Form.Text className="text-danger"></Form.Text>
+      </Form>
+    </Container>
   );
 };
 
