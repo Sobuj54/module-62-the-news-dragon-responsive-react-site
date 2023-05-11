f8ab7aa36c0a43c8135353f5e87e611232ebcedd
soubj <sobujahmed124@gmail.com>
2023-05-11 21:21:17 +0600

    created different navigation component

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,15 +1,10 @@
-import React, { useContext } from "react";
+import React from "react";
 import logo from "../../../assets/logo.png";
 import moment from "moment/moment";
-import { Button, Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
 import Marquee from "react-fast-marquee";
-import { Link } from "react-router-dom";
-import { FaUserCircle } from "react-icons/fa";
-import { AuthContext } from "../../../providers/AuthProvider";
 
 const Header = () => {
-  const { user } = useContext(AuthContext);
-
   return (
     <Container>
       <div className="text-center">
@@ -28,35 +23,6 @@ const Header = () => {
           text.
         </Marquee>
       </div>
-
-      <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
-        <Container>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="mx-auto">
-              <Nav.Link href="#features">
-                <Link to="/">Home</Link>
-              </Nav.Link>
-              <Nav.Link href="#pricing">About</Nav.Link>
-              <Nav.Link href="#pricing">Career</Nav.Link>
-            </Nav>
-            <Nav>
-              {user && (
-                <Nav.Link>
-                  <FaUserCircle style={{ fontSize: "1.5rem" }}></FaUserCircle>
-                </Nav.Link>
-              )}
-              <Nav.Link eventKey={2} href="#memes">
-                {user ? (
-                  <Button variant="secondary">Logout</Button>
-                ) : (
-                  <Link to="/login">Login</Link>
-                )}
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
     </Container>
   );
 };
